e13c6a49b74e5152150b2fb3bcdc89eeca730c49
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2024-06-27 20:47:22 -0500

    5-Parallel arrays completed.

--- src/main/java/com/pluralsight/calcengine/Main.java
+++ src/main/java/com/pluralsight/calcengine/Main.java
@@ -8,32 +8,33 @@ public class Main {
 
     public static void main(String[] args) {
 
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
 
-        double value1= 100.0d;
-        double value2 = 0.0d;
-        double result = 0.0d;
-        char opCode = 'd';
-
-
-        switch (opCode) {
+        for (int i = 0; i < opCodes.length; i++){
+        switch (opCodes[i]) {
             case 'a' :
-            result = value1 + value2;
+                results[i] = leftVals[i] + rightVals[i];
             break;
             case 's' :
-                result = value1 - value2;
-                break;
+                results[i] = leftVals[i] - rightVals[i];
+                    break;
             case 'm' :
-                result = value1 * value2;
+                results[i] = leftVals[i] * rightVals[i];
                 break;
             case  'd' :
-                result = value2 != 0 ? value1 / value2 : 0.0d;
+                results[i] = rightVals[i] != 0 ? leftVals[i] / rightVals[i] : 0.0d
                 break;
             default:
-                System.out.println("Invalid opCode: " + opCode);
-                result = 0.0d;
+                System.out.println("Invalid opCode: " + opCodes[i]);
+                results[i] = 0.0d;
             break;
+            
         }
-                System.out.println(result);
-
+    }
+                
+    System.out.println(results);
     }
 }
