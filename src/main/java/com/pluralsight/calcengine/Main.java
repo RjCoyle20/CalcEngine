53329d8f1fc9b0b67bcadf2ef79593a448de0842
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2024-08-12 20:19:26 -0500

    finished Creating an Array of Classes

--- src/main/java/com/pluralsight/calcengine/Main.java
+++ src/main/java/com/pluralsight/calcengine/Main.java
@@ -29,7 +29,35 @@ public class Main {
                 handleComandLine(args);
             } else 
                 System.out.println("Please provide an operation code and 2 numeric values");
-        } 
+        }
+
+
+    static void performCalculations() {
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
+
+        MathEquation [] equations = new MathEquation[4];
+        equations[0] = create (100.0d, 50.0d, 'd');
+        equations[1] = create (25.0d, 92.0d, 'a');
+        equations[2] = create (225.0d, 17.0d, 's');
+        equations[3] = create (11.0d, 3.0d, 'm');
+
+        for (int i = 0; i< opCodes.length; i++) {
+            results [i] =  execute(opCodes[i], leftVals[i], rightVals[i]);
+        }
+        for (double currentResult : results)
+            System.out.println(currentResult);
+    }
+
+    private static MathEquation create(double leftVal, double rightVal, char opCode){
+        MathEquation equation = new MathEquation();
+        equation.leftVal = leftVal;
+        equation.rightVal = rightVal;
+        equation.opCode = opCode;
+        return equation;
+        }
 
 
     static void executeInteractively() {
