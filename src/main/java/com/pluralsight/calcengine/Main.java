c59fe4e59e5376554dcce4fd6987cbbc43d93541
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2024-08-12 20:23:07 -0500

    Finished 'Using the MathEquation Class' from classes and objects

--- src/main/java/com/pluralsight/calcengine/Main.java
+++ src/main/java/com/pluralsight/calcengine/Main.java
@@ -33,10 +33,7 @@ public class Main {
 
 
     static void performCalculations() {
-        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-        char[] opCodes = {'d', 'a', 's', 'm'};
-        double[] results = new double[opCodes.length];
+
 
         MathEquation [] equations = new MathEquation[4];
         equations[0] = create (100.0d, 50.0d, 'd');
@@ -44,11 +41,10 @@ public class Main {
         equations[2] = create (225.0d, 17.0d, 's');
         equations[3] = create (11.0d, 3.0d, 'm');
 
-        for (int i = 0; i< opCodes.length; i++) {
-            results [i] =  execute(opCodes[i], leftVals[i], rightVals[i]);
+        for (MathEquation equation : equations) {
+            equation.execute();
+            System.out.println("result = " + equation.result);
         }
-        for (double currentResult : results)
-            System.out.println(currentResult);
     }
 
     private static MathEquation create(double leftVal, double rightVal, char opCode){
