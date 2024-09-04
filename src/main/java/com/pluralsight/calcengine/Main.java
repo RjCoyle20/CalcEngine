12a40c95fd7bf52179f6559aa8c8601b901b8cc9
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2024-09-03 21:03:47 -0500

    Finished course!

--- src/main/java/com/pluralsight/calcengine/Main.java
+++ src/main/java/com/pluralsight/calcengine/Main.java
@@ -29,12 +29,15 @@ public class Main {
 //                handleComandLine(args);
 //            } else
 //                System.out.println("Please provide an operation code and 2 numeric values");
-        if (args.length == 1 && args[0].equals("interactive"))
+        if(args.length == 0){
+            performCalculations();
+        } else if (args.length == 1 && args[0].equals("interactive"))
             executeInteractively();
-        else performCalculations();
-
-
-        }
+        else if (args.length == 3)
+            performOperation(args);
+        else
+            System.out.println("Please provide an operation code and 2 numberic values");
+       }
 
 
     static void performCalculations() {
@@ -91,70 +94,6 @@ public class Main {
         System.out.println(equation);
     }
 
-    private static void displayResult(char opCode, double leftVal, double rightVal, double result) {
-        char symbol = symbolFromOpCode(opCode);
-        StringBuilder builder = new StringBuilder(20);
-        builder.append(leftVal);
-        builder.append(" ");
-        builder.append(symbol);
-        builder.append(" ");
-        builder.append(rightVal);
-        builder.append(" = ");
-        builder.append(result);
-        String output = builder.toString();
-        System.out.println(output);
-    }
-    private static char symbolFromOpCode(char opCode) {
-        char[] opCodes = {'a', 's', 'm', 'd'};
-        char[] symbols = {'+', '-', '*', '/'};
-        char symbol = ' ';
-        for(int index = 0; index < opCodes.length; index++) {
-            if (opCode == opCodes[index]) {
-                symbol = symbols[index];
-                break;
-            }
-        }
-        return symbol;
-    }
-
-    private static void handleComandLine(String[] args) {
-        char opCode = args[0].charAt(0);
-        double leftVal = Double.parseDouble(args[1]);
-        double rightVal = Double.parseDouble(args[2]);
-        double result = execute(opCode, leftVal, rightVal);
-        System.out.println(result);
-    }
-
-
-
-    static double execute(char opCode, double leftVal, double rightVal) {
-        double result;
-        switch (opCode) {
-            case 'a' :
-                result = leftVal + rightVal;
-            break;
-            case 's' :
-            result = leftVal - rightVal;
-                    break;
-            case 'm' :
-            result = leftVal * rightVal;
-                break;
-            case  'd' :
-                result = rightVal != 0 ? leftVal / rightVal : 0.0d;
-                break;
-            default:
-                System.out.println("Invalid opCode: " + opCode);
-                result = 0.0d;
-            break;
-
-        }
-        return result;
-    }
-
-    static char opCodeFromString(String operationName) {
-        char opCode = operationName.charAt(0);
-        return opCode;
-    }
 
     static double valueFromWord (String word) {
 
